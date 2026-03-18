c4b2b876785b105551c68eb8cd4716fb58c32316
Harsh Gaur <hgaur701@gmail.com>
2026-03-19 00:09:24 +0530

    UPDATED: fixed issue #6

--- agent/src/commands.cpp
+++ agent/src/commands.cpp
@@ -3,7 +3,7 @@
 #include <string>	
 #include <winuser.h>
 
-void DisplayMessage(std::wstring title, std::wstring message) {
+void DisplayMessage(const std::wstring &title, const std::wstring &message) {
   MessageBoxW(NULL, message.c_str(), title.c_str(), MB_OK);
 }
 
