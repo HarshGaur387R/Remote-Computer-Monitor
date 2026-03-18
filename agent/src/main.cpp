12a9f935eb2335dc6124f3af6faee4319f39ae99
Harsh Gaur <hgaur701@gmail.com>
2026-03-18 18:33:57 +0530

    UPDATED: using ports from config file.

--- agent/src/main.cpp
+++ agent/src/main.cpp
@@ -1,11 +1,15 @@
+#include "config.h"
 #include "server.h"
 #include <iostream>
 using namespace std;
+
 int main() {
+  const int server_port = AGENT_PORT;
+  const int beacon_port = BEACON_PORT;
 
   cout << "This is Agent server!" << endl;
 
-  startServer(8080);
+  startServer(server_port);
 
   return 0;
 };
