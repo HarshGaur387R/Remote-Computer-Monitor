f09c136c446a4afc47e39722c78ef3a6c9a5094b
Harsh Gaur <hgaur701@gmail.com>
2026-03-22 16:48:20 +0530

    DELETE: After getting super furasted with linking and compiling errors of cpp i am removing cpp code. and this code will be replaced by GO

--- agent/include/commands.h
+++
@@ -1,9 +0,0 @@
-#ifndef COMMANDS_H
-#define COMMANDS_H
-#include <string>
-
-int LockScreen();
-int SendNotification(const std::wstring &title, const std::wstring &message);
-int DisplayMessage(const std::wstring &title, const std::wstring &message);
-
-#endif
