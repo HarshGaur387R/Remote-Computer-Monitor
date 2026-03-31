176c2e6143ab865785645a34f9817c0a5c4fdb7e
Harsh Gaur <hgaur701@gmail.com>
2026-04-01 01:17:32 +0530

    UPDATED: separated logic for setup and start

--- apps/agent-installer/rcm-installer.go
+++ apps/agent-installer/rcm-installer.go
@@ -5,8 +5,10 @@ import (
 	"encoding/hex"
 	"encoding/json"
 	"fmt"
+	"io"
 	"log"
 	"net"
+	"net/http"
 	"os"
 	"os/exec"
 	"strings"
@@ -62,9 +64,11 @@ func printHelp() {
 	fmt.Println("======================================")
 	fmt.Println()
 	fmt.Println("Usage:")
-	fmt.Println("  rcmaI <command>")
+	fmt.Println("  rcmai <command>")
 	fmt.Println()
 	fmt.Println("Available Commands:")
+	fmt.Println("  setup    Run this command before start command to make sure latest agent binary is installed")
+	fmt.Println()
 	fmt.Println("  start    Launches the agent service. If already running, no action is taken. If not running,")
 	fmt.Println("           the installer checks whether the agent is installed. If missing, the latest release")
 	fmt.Println("           is downloaded and installed before starting the service.")
@@ -104,37 +108,56 @@ func serviceExists(serviceName string) (bool, error) {
 	return true, nil
 }
 
-func startAgent() {
+func downloadAgent(url, dest string) error {
+	// 1. Make HTTP request
+	resp, err := http.Get(url)
+	if err != nil {
+		return fmt.Errorf("failed to download: %v", err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("bad status: %s", resp.Status)
+	}
+
+	// 2. Create destination file
+	out, err := os.Create(dest)
+	if err != nil {
+		return fmt.Errorf("failed to create file: %v", err)
+	}
+	defer out.Close()
+
+	// 3. Copy response body to file
+	_, err = io.Copy(out, resp.Body)
+	if err != nil {
+		return fmt.Errorf("failed to save file: %v", err)
+	}
+
+	return nil
+}
+
+func setupAgent() {
 	configDir := `C:\ProgramData\RCMAgent`
 	configFile := configDir + `\config.json`
-	serviceName := "RCMAgentService"
+	url := "https://github.com/HarshGaur387R/Remote-Computer-Monitor/tree/master/releases/agent/windows/latest/download/v1.0.0_rcma_testing.exe"
+	dest := `C:\Program Files\RCMAgent\rcma.exe`
 
-	// Chekcking if service exist.
-	isServiceExists, err := serviceExists(serviceName)
+	// 1) Create directory RCMAgent at C:\ProgramData\
+	if err := os.MkdirAll(configDir, 0755); err != nil {
+		log.Fatalf("Failed to create config directory: %v", err)
+		return
+	}
+
+	// 2) Create config file at C:\ProgramData\RCMAgent
+	f, err := os.Create(configFile)
 	if err != nil {
-		fmt.Println("Error on finding service:", err)
-		return
-	}
-	if !isServiceExists {
-		fmt.Println("Service not found, Try to install it again. run `rcmai --update`")
+		log.Fatalf("Failed to create config file: %v", err)
 		return
 	}
 
-	// Checking if service is running
-	running, err := isServiceRunning(serviceName)
-	if err != nil {
-		fmt.Println("Error checking service:", err)
-		return
-	}
-	if running {
-		fmt.Println("RCMAgentService is already running")
-		return
-	} else {
-		fmt.Println("RCMAgentService is not running")
-	}
+	defer f.Close()
 
-	return
-
+	// 3) Gather important resources to store in config.json
 	addr, lanError := getLANIP()
 	if lanError != nil {
 		log.Fatalf("Error on getting LANIP: %v", lanError)
@@ -150,23 +173,57 @@ func startAgent() {
 		log.Fatalf("Error on generating token: %v", tokenError)
 	}
 
-	// Ensure directory exists
-	if err := os.MkdirAll(configDir, 0755); err != nil {
-		log.Fatalf("Failed to create config directory: %v", err)
-	}
-
-	f, err := os.Create(configFile)
-	if err != nil {
-		log.Fatalf("Failed to create config file: %v", err)
-	}
-
-	defer f.Close()
-
+	// 4) Write down resources in config.json
 	cfg := Config{LANIP: addr, Port: port, AuthToken: token}
 	if err := json.NewEncoder(f).Encode(cfg); err != nil {
 		log.Fatalf("Failed to write config: %v", err)
 	}
 
+	// 5) Make directory at C:\Program Files\
+	path := `C:\Program Files\RCMAgent\`
+
+	if err := os.MkdirAll(path, 0755); err != nil {
+		log.Fatalf("failed to create directory %s: %v", path, err)
+		return
+	}
+
+	// 6) Now download agent binary in C:\Program Files\RCMAgent\
+	if err := downloadAgent(url, dest); err != nil {
+		fmt.Println("Error on downloading agent:", err)
+		fmt.Println("Please contact developer or repo maintanier.")
+		return
+	} else {
+		//TODO: Before moving forward check at the location if binary is available or not.
+		fmt.Println("Agent downloaded!")
+	}
+
+}
+
+func startAgent() {
+	serviceName := "RCMAgentService"
+
+	// Chekcking if service exist.
+	isServiceExists, err := serviceExists(serviceName)
+	if err != nil {
+		fmt.Println("Error on finding service:", err)
+		return
+	}
+	if !isServiceExists {
+		fmt.Println("Service agent not found. run command `rcmai setup` to install it.")
+	}
+
+	// Checking if service is running
+	running, err := isServiceRunning(serviceName)
+	if err != nil {
+		fmt.Println("Error checking service:", err)
+		return
+	}
+	if running {
+		fmt.Println("RCMAgentService is already running")
+		return
+	} else {
+		fmt.Println("RCMAgentService is not running")
+	}
 }
 
 func main() {
@@ -179,6 +236,8 @@ func main() {
 	switch os.Args[1] {
 	case "--help":
 		printHelp()
+	case "setup":
+		setupAgent()
 	case "start": // start service + show QR
 		startAgent()
 	case "stop": // stop service
@@ -187,5 +246,9 @@ func main() {
 		fmt.Println("Status is pressed")
 	case "restart": // stop + start
 		fmt.Println("Restart is pressed")
+	default:
+		fmt.Println(os.Args[1], "is not a command. run `rcmai --help` for further help")
 	}
+
+	fmt.Scanln()
 }
