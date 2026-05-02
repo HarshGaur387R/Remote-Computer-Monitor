ccac7b32f54bc45476264a98c67a40b18a9a5f1f
Harsh Gaur <hgaur701@gmail.com>
2026-05-02 12:17:48 +0530

    added project constant, new icon symbol

--- package/shared/src/project-constants.ts
+++ package/shared/src/project-constants.ts
@@ -1,2 +1,8 @@
 export const APP_NAME = "Remote Computer Monitor"
 export const APP_NAME_SHORT = "RCM"
+export const COLORS = {
+	primary: "#0F2854",
+	primary_light: "#1C4D8D",
+	secondery: "#4988C4",
+	secondery_light: "#BDE8F5"
+}
